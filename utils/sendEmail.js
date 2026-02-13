8394ca10d8ef64920e63729d06b72161bbfb85eb
priyanshujindal12 <priyanshujindal009@gmail.com>
2026-02-14 02:02:44 +0530

    fix bugs

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -7,11 +7,16 @@ const transporter = nodemailer.createTransport({
   },
 })
 const sendEmail = async (to, subject, text) => {
+  try{
   await transporter.sendMail({
     from: `"Angelix Security" <${process.env.EMAIL_USER}>`,
     to,
     subject,
     text,
   })
+  console.log("email send success")
+}catch(err){
+ console.log("error" ,err)
+}
 }
 module.exports = sendEmail
