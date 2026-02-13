0205e7bc16bbe828cf85b0b825811138558475a1
priyanshujindal12 <priyanshujindal009@gmail.com>
2026-02-14 01:43:44 +0530

    update bugs

--- controllers/OtpController.js
+++ controllers/OtpController.js
@@ -5,7 +5,9 @@ const OtpModel = require('../models/OtpModel')
 const sendOtp = async (req, res) => {
   try {
     const { clerkId } = req.auth
-    const user = await User.findOne({ clerkId })
+    console.log("request came");
+    const user = await User.findOne({ clerkId });
+    console.log(user.clerkId);
     if (!user) {
       return res.status(404).json({ message: 'User not found' })
     }
@@ -17,18 +19,20 @@ const sendOtp = async (req, res) => {
       otp,
       expiresAt: new Date(Date.now() + 5 * 60 * 1000),
     })
+    console.log("otp created succesfully");
     await sendEmail(
       user.email,
       'Angelix Phone Verification OTP',
       `Your OTP is ${otp}. It expires in 5 minutes.`
     )
+    console.log("emial send sucessfulyy")
     res.status(200).json({
       success: true,
       message: 'OTP sent to your email',
     })
   } catch (error) {
     console.error('Send OTP error:', error.message)
-    res.status(500).json({ message: 'Failed to send OTP' })
+    res.status(500).json({ message: 'Failed to send OTP', error: error.message })
   }
 }
 const verifyOtp = async (req, res) => {
